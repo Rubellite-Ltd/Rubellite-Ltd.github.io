95a9cfa765a9be9aa0577828f4c54fbe239fd45d
Rubellite <67942706+Trdsphr@users.noreply.github.com>
2024-07-28 06:24:39 +0100

    Update submit-form.js

--- Contact/netlify/Functions/submit-form.js
+++ Contact/netlify/Functions/submit-form.js
@@ -1,37 +1,57 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 exports.handler = async function(event, context) {
     if (event.httpMethod !== 'POST') {
         return {
             statusCode: 405,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'POST',
+            },
             body: 'Method Not Allowed',
         };
     }
 
     const data = JSON.parse(event.body);
 
-    const client = new MongoClient(process.env.MONGODB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-
     try {
+        // Connect the client to the server (optional starting in v4.7)
         await client.connect();
-        const database = client.db('contactudbname');
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+        const database = client.db('contactudbname'); // Replace 'your-database-name' with your actual database name
         const collection = database.collection('contactuscollection');
         await collection.insertOne(data);
 
         return {
             statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
             body: 'Form submitted successfully',
         };
     } catch (error) {
-        console.error('Error connecting to MongoDB', error);
+        console.error('Error connecting to MongoDB:', error);
         return {
             statusCode: 500,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
             body: 'Internal Server Error',
         };
     } finally {
+        // Ensures that the client will close when you finish/error
         await client.close();
     }
 };
